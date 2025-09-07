7b7bf8da15f8c83bca3a8ac55a0c4c5304ed4332
mgutierrez <mgutierrez>
2025-09-07 20:41:06 -0300

    Agrego clases para leer archivos CSV

---
+++ src/main/java/Utils/ClienteCsvLoader.java
@@ -0,0 +1,42 @@
+package Utils;
+
+import DAO.ClienteDAO;
+import entities.Cliente;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class ClienteCsvLoader {
+    private final ClienteDAO clienteDAO;
+
+    public ClienteCsvLoader(ClienteDAO clienteDAO) {
+        this.clienteDAO = clienteDAO;
+    }
+
+    public void load(String filePath) {
+        try (FileReader reader = new FileReader(filePath, StandardCharsets.UTF_8);
+             CSVParser parser = CSVFormat.DEFAULT
+                     .withFirstRecordAsHeader()
+                     .parse(reader)) {
+
+            for (CSVRecord row : parser) {
+                int idCliente = Integer.parseInt(row.get("idCliente"));
+                String nombre = row.get("nombre");
+                String email = row.get("email");
+
+                Cliente c = new Cliente(idCliente, nombre, email);
+                clienteDAO.crearCliente(c);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/Utils/ProductoCsvLoader.java
@@ -0,0 +1,40 @@
+package Utils;
+
+import DAO.ProductoDAO;
+import entities.Producto;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+
+public class ProductoCsvLoader {
+    private final ProductoDAO productoDAO;
+
+    public ProductoCsvLoader(ProductoDAO productoDAO) {
+        this.productoDAO = productoDAO;
+    }
+
+    public void load(String filePath) {
+        try (FileReader reader = new FileReader(filePath, StandardCharsets.UTF_8);
+             CSVParser parser = CSVFormat.DEFAULT
+                     .withFirstRecordAsHeader()
+                     .parse(reader)) {
+
+            for (CSVRecord row : parser) {
+                int idProducto = Integer.parseInt(row.get("idProducto"));
+                String nombre = row.get("nombre");
+                float valor = Float.parseFloat(row.get("valor"));
+
+                Producto p = new Producto(idProducto, nombre, valor);
+                productoDAO.crearProducto(p); // persistimos en la BD
+            }
+            System.out.println("Productos cargados desde " + filePath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
