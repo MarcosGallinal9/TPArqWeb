8fcb55cfef22e86356649eda0bb72a587221ac6f
mgutierrez <mgutierrez>
2025-10-01 18:51:43 -0300

    Modifico archivos para poder ejecutar

--- integrador_2/src/main/java/modelo/Carrera.java
+++ integrador_2/src/main/java/modelo/Carrera.java
@@ -1,21 +1,4 @@
 package modelo;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
-import java.util.List;
-
-@Entity
-@Table(name = "Carrera")
-@Data
 public class Carrera {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id_carrera;
-
-    private String carrera;
-    private int duracion; // En años
-
-    @OneToMany(mappedBy = "carrera")
-    private List<EstudianteCarrera> estudiantesCarrera;
 }
