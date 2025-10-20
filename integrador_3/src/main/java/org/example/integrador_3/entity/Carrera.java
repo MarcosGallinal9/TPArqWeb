e7dc352326a35e66f7dc3a534265b2da0b5b992e
mgutierrez <mgutierrez>
2025-10-20 17:37:48 -0300

    Agrego funcionalidad para leer csv

--- integrador_3/src/main/java/org/example/integrador_3/entity/Carrera.java
+++ integrador_3/src/main/java/org/example/integrador_3/entity/Carrera.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class Carrera {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idCarrera;
 
     private String carrera;
