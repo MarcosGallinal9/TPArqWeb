720cfc802fd6e9dc10181416829992589657fbbc
mgutierrez <mgutierrez>
2025-11-05 21:06:48 -0300

    microservicio Monopatin andando en Mongo Atlas

--- tp_integrador/monopatin/src/main/java/org/example/monopatin/entity/Monopatin.java
+++ tp_integrador/monopatin/src/main/java/org/example/monopatin/entity/Monopatin.java
@@ -1,7 +1,7 @@
 package org.example.monopatin.entity;
 
 
-import jakarta.persistence.Id;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
