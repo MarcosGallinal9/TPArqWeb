40e77f664dad3d37181f460423c9ae481317fe83
alfonmoris <amoris@alumnos.exa.unicen.edu.ar>
2025-11-06 15:53:27 -0300

    avanzo

--- tp_integrador/usuario/src/main/java/repository/UsuarioRepository.java
+++ tp_integrador/usuario/src/main/java/repository/UsuarioRepository.java
@@ -1,10 +1,12 @@
 package repository;
 
 import entity.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Long>{
-    List<Monopatin> findByUserId(Long userId);
+public interface UsuarioRepository extends MongoRepository<Usuario,Long>{
+    List<Integer> findByUserId(Long userId);
 }
