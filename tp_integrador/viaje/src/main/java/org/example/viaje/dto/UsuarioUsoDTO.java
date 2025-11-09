1c401ff8783ecc0b2c8c589b53aeb47f0dd17b5a
mgutierrez <mgutierrez>
2025-11-09 20:50:28 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ tp_integrador/viaje/src/main/java/org/example/viaje/dto/UsuarioUsoDTO.java
@@ -0,0 +1,15 @@
+package org.example.viaje.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioUsoDTO {
+    private String id;
+    private String nombre;
+    private String rol;
+    private Double kmRecorridos;
+}
